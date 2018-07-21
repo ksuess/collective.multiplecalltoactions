04640886985e6ffbd228eb49d49b8d21c346d549
Katja Suess <k.suess@rohberg.ch>
2018-07-21 21:14:54 +0200

    "Add behavior: CallToActionBehavior"

---
+++ src/collective/calltoaction/behaviors/call_to_action_behavior.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+from collective.calltoaction import _
+from plone import schema
+from plone.autoform.interfaces import IFormFieldProvider
+from plone.dexterity.interfaces import IDexterityContent
+from plone.supermodel import model
+from zope.component import adapter
+from zope.interface import implementer
+from zope.interface import provider
+
+
+@provider(IFormFieldProvider)
+class ICallToActionBehavior(model.Schema):
+    """
+    """
+
+    project = schema.TextLine(
+        title=_(u'Project'),
+        description=_(u'Give in a project name'),
+        required=False,
+    )
+
+
+@implementer(ICallToActionBehavior)
+@adapter(IDexterityContent)
+class CallToActionBehavior(object):
+    def __init__(self, context):
+        self.context = context
+
+    @property
+    def project(self):
+        if hasattr(self.context, 'project'):
+            return self.context.project
+        return None
+
+    @project.setter
+    def project(self, value):
+        self.context.project = value
